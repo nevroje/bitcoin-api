5bb3a1a47a22f7ffd9387f2cf25e97f6bd76f35a
nevroje <joakim.berglind@gmail.com>
2019-06-07 12:02:28 +0200

    installed material ui , chartjs created wip logic for charts and getting json data response

--- src/App.js
+++ src/App.js
@@ -1,10 +1,80 @@
-import React from 'react';
+import React, { Component } from "react";
+import { render } from "react-dom";
+import { withStyles } from "@material-ui/core/styles";
+import Chart from "./chart";
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+const styles = theme => ({
+  "chart-container": {
+    height: 400
+  }
+});
+
+class App extends Component() {
+  state = {
+    lineChartData: {
+      labels: [],
+      datasets: [
+        {
+          type: "line",
+          label: "BTC-USD",
+          backgroundColor: "rgba(0, 0, 0, 0)",
+          borderColor: this.props.theme.palette.primary.main,
+          pointBackgroundColor: this.props.theme.palette.secondary.main,
+          pointBorderColor: this.props.theme.palette.secondary.main,
+          borderWidth: "2",
+          lineTension: 0.45,
+          data: []
+        }
+      ]
+    },
+    lineChartOptions: {
+      responsive: true,
+      mainstainAspectRatio: false,
+      tooltips: {
+        enabled: true
+      },
+      scales: {
+        xAxes: [
+          {
+            ticks: {
+              autoSkip: true,
+              maxTicksLimit:10
+            }
+          }
+        ]
+      }
+    }
+  };
+
+  componentDidMount() {
+    const subscribe = {
+      type: "subscribe",
+      channels: [
+        {
+          name: "ticker",
+          product_ids: ["BTC-USD"]
+        }
+      ]
+    }
+
+    this.ws = new WebSocket("wss://ws-feed.gdax.com");
+
+    this.ws.onopen = () => {
+      this.ws.send(JSON.stringify(subscribe))
+    };
+    this.ws.onmessage = e => {
+      const value = JSON.parse(e.data);
+      if (value.type !== "ticker") {
+        return;
+      }
+
+      const oldBtcDataSet = this.state.lineChartData.datasets[0];
+      const newBtcDataSet = {...oldBtcDataSet};
+      newBtcDataSet.data.push(value.price);
+
+      const newChartData
+    }
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles";
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const theme = createMuiTheme();
+
+ReactDOM.render(
+  <MuiThemeProvider theme={theme}>
+    <App />
+  </MuiThemeProvider>,
+  document.getElementById("root")
+);
 serviceWorker.unregister();
